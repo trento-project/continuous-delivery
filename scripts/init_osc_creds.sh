70b6e559cfda2b272e8f416fb2c210be3aab40a6
Stefano Torresi <stefano.torresi@suse.com>
2024-12-16 20:54:01 +0100

    provide a more portable way to initialize the osc config
    this is mainly due to GitHub Actions overriding the $HOME directory

--- scripts/init_osc_creds.sh
+++ scripts/init_osc_creds.sh
@@ -5,4 +5,9 @@ source $(dirname $0)/utils.sh
 
 OSCRC_FILE=${OSCRC_FILE:=$HOME/.config/osc/oscrc}
 
+if [ ! -f "$OSCRC_FILE" ]; then
+  mkdir -p $(dirname $OSCRC_FILE)
+  cp /usr/local/share/osc/oscrc.sample $OSCRC_FILE
+fi
+
 check_user
